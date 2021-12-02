2cedf88f87cc11ad3c4aab8e6808412a57495294
Joshwasson92 <93554057+Joshwasson92@users.noreply.github.com>
2021-12-02 14:00:34 -0500

    Reefodex
    Added coral types in an array
    assigned values to each.

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,7 @@
-alert('Hello World');
-let favoriteFood = 'Greek';
-document.write(favoriteFood);
+let reefList =[
+    {name: 'Green Star Polyp', height: 2, type: ['Soft Coral', 'Aggressive', 'Non-Toxic' ] },
+    {name: 'Green Sinularia', height: 8, type: ['Soft Coral', 'Peaceful', 'Non-Toxic']},
+    {name: 'Frogspawn Coral', height: 4,  type: ['Hard Coral', 'Aggressive', 'Non-Toxic'] },
+    {name: 'Palythoa', height: 3, type: ['Soft Coral', 'Peaceful', 'Toxic'] },
+    {name: 'Acropora', height: 10, type: ['Hard Coral', 'Peaceful', 'Non-Toxic']},
+]
