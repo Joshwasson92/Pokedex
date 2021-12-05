0dfee34c5a2fafe06a3b138254ecb6acd1c2c364
Joshwasson92 <93554057+Joshwasson92@users.noreply.github.com>
2021-12-05 08:08:40 -0500

    Updated list
    Changed list from reef animals to water pokemon to allow the use of a external API

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,7 @@
-let reefList =[
-    {name: 'Green Star Polyp', height: 2, type: ['Soft Coral', 'Aggressive', 'Non-Toxic' ] },
-    {name: 'Green Sinularia', height: 8, type: ['Soft Coral', 'Peaceful', 'Non-Toxic']},
-    {name: 'Frogspawn Coral', height: 4,  type: ['Hard Coral', 'Aggressive', 'Non-Toxic'] },
-    {name: 'Palythoa', height: 3, type: ['Soft Coral', 'Peaceful', 'Toxic'] },
-    {name: 'Acropora', height: 10, type: ['Hard Coral', 'Peaceful', 'Non-Toxic']},
+let pokeList =[
+    {name: 'Tentacruel', height: 5, type: ['Water', 'Poison']},
+    {name: 'Seadra', height: 4, type: ['Water']},
+    {name: 'Starmie', height: 3,  type: ['Water', 'Psychic']},
+    {name: 'Seaking', height: 4, type: ['Water']},
+    {name: 'Lapras', height: 8, type: ['Water', 'Ice']},
 ]
