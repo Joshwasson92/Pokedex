d12de90da6b7f2b63bd86026519448f9c7509f51
Joshwasson92 <93554057+Joshwasson92@users.noreply.github.com>
2021-12-06 18:53:07 -0500

    1.3 update
    Created variables for Pokemon Name and Height  to use in the for loop and also created several if statements for document.write.
    added basic css formatting. will change font when finished visualizing layout.

--- js/scripts.js
+++ js/scripts.js
@@ -5,3 +5,29 @@ let pokeList =[
     {name: 'Seaking', height: 4, type: ['Water']},
     {name: 'Lapras', height: 8, type: ['Water', 'Ice']},
 ]
+
+for (let i = 0; i < pokeList.length; i++) {
+    let height = pokeList[i].height
+    let name = pokeList[i].name
+    let pokemonInfo = `<p> Name: ${name} Height: ${height}`
+    //Template String - 
+    if (height > 4) {
+     pokemonInfo += " That is a large Pokemon! "
+    } 
+    if (height < 4) {
+        pokemonInfo += " That is a small Pokemon! "
+       } 
+       if (height === 4) {
+        pokemonInfo += " That is an average sized Pokemon! "
+       } 
+    pokemonInfo += "</p>"
+    document.write(pokemonInfo);
+}
+  
+ 
+//  test while loop
+// let j=0; 
+// while (j < pokeList.length){
+//     document.write(`Name: ${pokeList[j].name} Height: ${pokeList[j].height}`);
+//     j++
+// }
