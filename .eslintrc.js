8f5571edb92729133d4d90830e5e01e49249878b
Joshwasson92 <93554057+Joshwasson92@users.noreply.github.com>
2022-01-18 16:14:47 -0500

    Update .eslintrc.js
    Added jquery support to eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,7 +1,10 @@
 module.exports = {
     "env": {
         "browser": true,
-        "es2021": true
+        "es2021": true,
+        "commonjs": true,
+        "es6": true,
+        "jquery": true
     },
     "extends": "eslint:recommended",
     "parserOptions": {
